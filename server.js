818d9c7935a92cc1e46dac27537895e0adfd9d49
bas.pieren@quicknet.nl <bas.pieren@quicknet.nl>
2019-04-04 11:37:19 +0200

    ✨ Added the ability to filter on sound and light

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 const express = require('express'),
       request = require('request'),
       compression = require('compression'),
+      bodyParser = require('body-parser'),
       app = express(),
       port = 3000
 
@@ -11,9 +12,12 @@ app
   .set('views', 'views')
 
   .use(express.static('public'))
+  .use(bodyParser.urlencoded({ extended: false }))
   .use(compression())
 
   .get('/', homePage)
+
+  .post('/filter', filterHomePage)
 
   .listen(process.env.PORT || port)
 
@@ -29,6 +33,36 @@ function homePage(req, res) {
     // console.log(cleanedData)
 
     res.render('pages/index.ejs', {data: cleanedData})
+  })
+}
+
+function filterHomePage(req, res) {
+  request('http://mirabeau.denniswegereef.nl/api/v1/rooms', (error, response, body) => {
+    console.log('error:', error)
+    console.log('statusCode:', response && response.statusCode)
+    // console.log('body:', body)
+
+    let filterValue = req.body.filter,
+        data = JSON.parse(body),
+        cleanedData = cleanData(data),
+        filterData = cleanedData.filter(e => {
+          if (filterValue === 'sound' && e.measurements.occupancy === false) {
+            return true
+          } else if (filterValue === 'light' && e.measurements.occupancy === false) {
+            return true
+          }
+        }),
+        sortData = filterData.sort((a, b) => {
+          if (filterValue === 'sound') {
+            return a.measurements.mic_level - b.measurements.mic_level
+          } else if (filterValue === 'light'){
+            return a.measurements.ambient_light - b.measurements.ambient_light
+          }
+        })
+
+    // console.log(sortData)
+
+    res.render('pages/filter.ejs', {data: sortData})
   })
 }
 
@@ -49,11 +83,10 @@ function cleanData(e) {
               occupancy: e.measurements.occupancy
           }
         }
+      }),
+      sortOccupancy = mapData.sort((a, b) => {
+        return (a.measurements.occupancy === b.measurements.occupancy) ? 0 : a.measurements.occupancy ? 1 : -1
       })
-
-  let sortOccupancy = mapData.sort((a, b) => {
-    return (a.measurements.occupancy === b.measurements.occupancy) ? 0 : a.measurements.occupancy ? 1 : -1
-  })
 
   return sortOccupancy
 }
